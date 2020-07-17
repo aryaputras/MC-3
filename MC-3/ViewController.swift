9c715875c76f1ee42a8a9d28d5611c797942aea6
aryaputras <62135307+aryaputras@users.noreply.github.com>
2020-07-17 15:24:58 +0700

    1

---
+++ MC-3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC-3
+//
+//  Created by Abigail Aryaputra Sudarman on 17/07/20.
+//  Copyright © 2020 Abigail Aryaputra Sudarman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
