ff47f6c6515ad593fba2339651d545b41daab755
aryaputras <62135307+aryaputras@users.noreply.github.com>
2020-07-19 22:25:08 +0700

    Added saving profile features but not age preference

--- MC-3/Register2VC.swift
+++ MC-3/Register2VC.swift
@@ -7,25 +7,94 @@
 //
 
 import UIKit
+import CloudKit
 
 class Register2VC: UIViewController {
-    var GenderPrefer : String = ""
+    var genderPrefer : [Int] = []
+    var agePrefer : [Int] = []
+    
+    
+    var age = 0
+    var name = ""
+    var gender = 0
+    var avatar = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+ 
 
 
     }
     
-    @IBAction func ButtonMale(_ sender: Any) {
-        GenderPrefer = "Laki-Laki"
+    @IBAction func buttonMale(_ sender: Any) {
+        genderPrefer = [0]
     }
     
-    @IBAction func ButtonFemale(_ sender: Any) {
-        GenderPrefer = "Perempuan"
+    @IBAction func buttonFemale(_ sender: Any) {
+        genderPrefer = [1]
     }
     
-    @IBAction func ButtonAll(_ sender: Any) {
-        GenderPrefer = "Semua"
+    @IBAction func buttonAll(_ sender: Any) {
+        genderPrefer = [0,1]
+    }
+
+   
+
+    @IBAction func buttonNext(_ sender: Any) {
+        
+          CKContainer.default().fetchUserRecordID { userID, error in
+        if let userID = userID {
+            //print(userID)
+            let nameRecord = self.name as CKRecordValue
+            let ageRecord = self.age as CKRecordValue
+            let genderRecord = self.gender as CKRecordValue
+            let agePreferRecord = self.agePrefer as CKRecordValue
+            let genderPreferRecord = self.genderPrefer as CKRecordValue
+            let avatarRecord = self.avatar as CKRecordValue
+            let creatorID = userID.recordName as CKRecordValue
+            let date = Date() as CKRecordValue
+           
+          
+
+                            
+                let newRecord = CKRecord(recordType: "profile")
+                
+                let database = CKContainer.default().publicCloudDatabase
+                
+                //newRecord.setObject(avatar, forKey: "regisimage")
+                newRecord.setObject(nameRecord, forKey: "username")
+                newRecord.setObject(ageRecord, forKey: "age")
+                newRecord.setObject(genderRecord, forKey: "gender")
+                newRecord.setObject(agePreferRecord, forKey: "agePreference")
+                newRecord.setObject(genderPreferRecord, forKey: "genderPreference")
+                newRecord.setObject(avatarRecord, forKey: "avatar")
+                newRecord.setObject(creatorID, forKey: "creatorID")
+                newRecord.setObject(date, forKey: "signUpDate")
+                
+                database.save(newRecord) { record , error in
+                    DispatchQueue.main.async {
+                        if let error = error {
+                            print("error")
+                            
+                        } else {
+                            print("record was saved")
+                          
+                        }
+                    }
+                }
+            } }
+        
+       
+        
     }
     
-}
+           
+           
+//
+
+//
+//
+
+                   }
+              
