e80e1b451668e87c88b363f46b997066b8c4b73e
DrewNovansky <novanskyi@gmail.com>
2020-07-21 12:56:46 +0700

    Frontend
    nyatuin halaman storyboard nulis surat

---
+++ MC-3/SendAnimationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SendAnimationViewController.swift
+//  MC-3
+//
+//  Created by Andrew Novansky Ignatius on 21/07/20.
+//  Copyright © 2020 Abigail Aryaputra Sudarman. All rights reserved.
+//
+
+import UIKit
+
+class SendAnimationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
