d854c90d86bb0208cc727cade1f554d5adc60206
DrewNovansky <novanskyi@gmail.com>
2020-07-21 12:26:05 +0700

    Front End menulis surat dan animasi

---
+++ MC-3/RiwayatCollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  RiwayatCollectionCell.swift
+//  MC-3
+//
+//  Created by Andrew Novansky Ignatius on 20/07/20.
+//  Copyright © 2020 Abigail Aryaputra Sudarman. All rights reserved.
+//
+
+import UIKit
+
+class RiwayatCollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
