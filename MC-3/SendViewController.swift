275bbcf58324db0edf74c2fd111bded812bd7bb8
aryaputras <62135307+aryaputras@users.noreply.github.com>
2020-07-24 15:01:39 +0700

    reply capabilities

--- MC-3/SendViewController.swift
+++ MC-3/SendViewController.swift
@@ -62,6 +62,41 @@ class SendViewController: UIViewController {
         record2Label.isHidden = true
         recordingButton.isHidden = true
 
+        
+            CKContainer.default().fetchUserRecordID { userID, error in
+                     if let userID = userID {
+                        let database = CKContainer.default().publicCloudDatabase
+                        
+                        let predicate = NSPredicate(format: "creatorID == %@", userID.recordName)
+                        let queryProfile = CKQuery(recordType: "profile", predicate: predicate)
+                        queryProfile.sortDescriptors = [NSSortDescriptor(key: "signUpDate", ascending: false)]
+                        
+                        database.perform(queryProfile, inZoneWith: nil) { (records, error) in
+                               if let fetchedRecords = records {
+                                   self.profile = fetchedRecords
+                                   DispatchQueue.main.async {
+                                     //get sender age
+                                    self.age = self.profile[0].object(forKey: "age") as! Int
+                                   
+                                    
+                                    
+                                    //get sender gender
+                                    self.gender = self.profile[0].object(forKey: "gender") as! Int
+                                   
+                                    
+                                      
+                                       
+                                   }
+                                   
+                               }
+                           }
+                        
+                        
+                }
+                
+        }
+        
+        
     }
     
     @IBAction func recordButton(_ sender: Any) {
@@ -136,38 +171,7 @@ class SendViewController: UIViewController {
         canvasView.strokeColor = #colorLiteral(red: 0.5280317664, green: 0.1064086631, blue: 0.7941021323, alpha: 1)
 
         //fetching age
-        CKContainer.default().fetchUserRecordID { userID, error in
-                 if let userID = userID {
-                    let database = CKContainer.default().publicCloudDatabase
-                    
-                    let predicate = NSPredicate(format: "creatorID == %@", userID.recordName)
-                    let queryProfile = CKQuery(recordType: "profile", predicate: predicate)
-                    queryProfile.sortDescriptors = [NSSortDescriptor(key: "signUpDate", ascending: false)]
-                    
-                    database.perform(queryProfile, inZoneWith: nil) { (records, error) in
-                           if let fetchedRecords = records {
-                               self.profile = fetchedRecords
-                               DispatchQueue.main.async {
-                                 //get sender age
-                                self.age = self.profile[0].object(forKey: "age") as! Int
-                               
-                                
-                                
-                                //get sender gender
-                                self.gender = self.profile[0].object(forKey: "gender") as! Int
-                               
-                                
-                                  
-                                   
-                               }
-                               
-                           }
-                       }
-                    
-                    
-            }
-            
-    }
+
     }
     
     @IBAction func doneAction(_ sender: Any) {
