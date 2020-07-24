ce4ae25ebaa1f3f7c65a6484ff21fa43512a9744
aryaputras <62135307+aryaputras@users.noreply.github.com>
2020-07-24 16:11:18 +0700

    1

---
+++ MC-3/MencariViewController.swift
@@ -0,0 +1,68 @@
+import Foundation
+import UIKit
+import AVFoundation
+import CoreAudio
+
+class MencariViewController: UIViewController {
+
+    var recorder: AVAudioRecorder!
+    var levelTimer = Timer()
+
+    let LEVEL_THRESHOLD: Float = -10.0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let documents = URL(fileURLWithPath: NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0])
+        let url = documents.appendingPathComponent("record.caf")
+
+        let recordSettings: [String: Any] = [
+            AVFormatIDKey:              kAudioFormatAppleIMA4,
+            AVSampleRateKey:            44100.0,
+            AVNumberOfChannelsKey:      2,
+            AVEncoderBitRateKey:        12800,
+            AVLinearPCMBitDepthKey:     16,
+            AVEncoderAudioQualityKey:   AVAudioQuality.max.rawValue
+        ]
+
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.playAndRecord)
+            try audioSession.setActive(true)
+            try recorder = AVAudioRecorder(url:url, settings: recordSettings)
+
+        } catch {
+            return
+        }
+
+        recorder.prepareToRecord()
+        recorder.isMeteringEnabled = true
+        recorder.record()
+
+        levelTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(levelTimerCallback), userInfo: nil, repeats: true)
+
+    }
+
+    @objc func levelTimerCallback() {
+        recorder.updateMeters()
+
+        let level = recorder.averagePower(forChannel: 0)
+        let isLoud = level > LEVEL_THRESHOLD
+        
+        if isLoud == true {
+            print("true")
+            
+        } else {
+          
+        }
+
+        // do whatever you want with isLoud
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
